173b14e99e6aaabfeca7414478b7109bf9a1a6e9
David Chen <dche6963@Uni.sydney.edu.au>
2018-09-25 20:42:35 +1000

    missing files re-added

--- build/scripts/Soft2412-Calculator.bat
+++ build/scripts/Soft2412-Calculator.bat
@@ -1,7 +1,7 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  Group13 startup script for Windows
+@rem  Soft2412-Calculator startup script for Windows
 @rem
 @rem ##########################################################################
 
@@ -13,7 +13,7 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%..
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GROUP13_OPTS to pass JVM options to this script.
+@rem Add default JVM options here. You can also use JAVA_OPTS and SOFT2412_CALCULATOR_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
@@ -63,19 +63,19 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\Group13.jar;%APP_HOME%\lib\guava-23.0.jar;%APP_HOME%\lib\jsr305-1.3.9.jar;%APP_HOME%\lib\error_prone_annotations-2.0.18.jar;%APP_HOME%\lib\j2objc-annotations-1.1.jar;%APP_HOME%\lib\animal-sniffer-annotations-1.14.jar
+set CLASSPATH=%APP_HOME%\lib\Soft2412-Calculator.jar;%APP_HOME%\lib\guava-23.0.jar;%APP_HOME%\lib\jsr305-1.3.9.jar;%APP_HOME%\lib\error_prone_annotations-2.0.18.jar;%APP_HOME%\lib\j2objc-annotations-1.1.jar;%APP_HOME%\lib\animal-sniffer-annotations-1.14.jar
 
-@rem Execute Group13
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GROUP13_OPTS%  -classpath "%CLASSPATH%" App %CMD_LINE_ARGS%
+@rem Execute Soft2412-Calculator
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SOFT2412_CALCULATOR_OPTS%  -classpath "%CLASSPATH%" App %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable GROUP13_EXIT_CONSOLE if you need the _script_ return code instead of
+rem Set variable SOFT2412_CALCULATOR_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not "" == "%GROUP13_EXIT_CONSOLE%" exit 1
+if  not "" == "%SOFT2412_CALCULATOR_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
